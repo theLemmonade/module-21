79c2891c4d647fdf38bde7a78ab9c668bee436cf
theLemmonade <lemmonmail@icloud.com>
2023-03-08 15:33:18 -0500

    added apollo provider to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ const authLink = setContext((_, { headers }) => {
 		headers: {
 			...headers,
 			authorization: token ? `Bearer ${token}` : "",
-		},
+		},  
 	};
 });
 
@@ -32,16 +32,18 @@ const client = new ApolloClient({
 
 function App() {
 	return (
+    <ApolloProvider client={ client }>
 		<Router>
 			<>
 				<Navbar />
 				<Switch>
-					<Route exact path="/" component={SearchBooks} />
-					<Route exact path="/saved" component={SavedBooks} />
+					<Route exact path="/" component={ SearchBooks } />
+					<Route exact path="/saved" component={ SavedBooks } />
 					<Route render={() => <h1 className="display-2">Wrong page!</h1>} />
 				</Switch>
 			</>
 		</Router>
+    </ApolloProvider>
 	);
 }
 
